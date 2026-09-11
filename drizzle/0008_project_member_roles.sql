fe14c0de3ea898e33fc08b753e7acb4973212cac
mt1979nc <marisa@pimsyehr.com>
2026-09-11 05:11:00 -0700

    feat: v1.8.1 timeline from kickoff/go-live, slip push, Dock roles

---
+++ drizzle/0008_project_member_roles.sql
@@ -0,0 +1,3 @@
+ALTER TYPE "public"."project_member_role" ADD VALUE 'SPECIALIST';--> statement-breakpoint
+ALTER TYPE "public"."project_member_role" ADD VALUE 'RCM';--> statement-breakpoint
+ALTER TYPE "public"."project_member_role" ADD VALUE 'BILLING_SUPPORT';
